62f86b20f72c8ac4f8d32dc8fd9625a490b673a0
Olepeponcio <olepepinsan@gmail.com>
2025-09-19 08:33:26 +0200

    Creation of the program flow estructure

---
+++ main.py
@@ -0,0 +1,52 @@
+"""Main class. Hosts the structure of the game's main loop.
+Imports and calls the classes defined in Object-Oriented Programming."""
+# importar módulos y librerias
+
+# START PROGRAM
+
+# Initialize Pygame
+
+# Create window (scree)
+
+# Define colors and fonts
+
+# Set up clock (FPS)
+
+# Initialize snake and initial food position
+
+# MAIN LOOP
+
+    # captura events (keyborad)
+    # Direction key pressed?
+        # yes -> update direction
+
+    # Move snake in current direction
+    # Add new "head" forward path
+    # Remove last "tail"
+    # snake collides with food?
+
+        # NO
+        # YES
+            # Generate new food at random position
+            # Increase snake length
+            #Increase score
+
+    # Sanake collides with walls or itself
+        # NO
+        # YES
+            #GAME OVER
+                #SHOW FINAL SCORE
+                #Pause/restart
+
+    #UPDATE GUID
+        #Draw snak, food
+        # Display score
+        # refresh screen
+        # control FPS
+
+        # Back to MAIN LOOP
+
+
+
+
+
